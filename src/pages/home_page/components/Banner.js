e59804059484e3d643a93d6e8e4720367ce529e4
000NINTENDO <nitensolanki02@gmail.com>
2021-02-23 22:52:29 +0530

    some fixes in banner layer and Banner comnponent is edited

--- src/pages/home_page/components/Banner.js
+++ src/pages/home_page/components/Banner.js
@@ -54,7 +54,7 @@ const Banner = () => {
 	console.log("isLoading", isLoading);
 
 	useEffect(() => {
-		const randomPageNumber = randomPagePicker(3);
+		const randomPageNumber = randomPagePicker(10);
 		console.log("randomPageNumber", randomPageNumber);
 
 		const fetchTopTatedMovies = async () => {
