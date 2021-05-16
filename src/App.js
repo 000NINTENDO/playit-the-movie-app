7e6903fd105d640fb0c2397786669bf72d8770c4
Niten Solanki <74289995+000NINTENDO@users.noreply.github.com>
2021-05-16 10:33:15 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import HomePage from "./pages/home_page/HomePage";
 import MoviePage from "./pages/movie_page/MoviePage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { MovieTrailerProvider } from "./context/MovieTrailerContext";
+import "./fonts/fonts.css";
 
 function App() {
-	return (
-		<div className="App">
-			<MovieTrailerProvider>
-				<BrowserRouter>
-					<Switch>
-						<Route exact path="/">
-							<HomePage />
-						</Route>
-						<Route path="/movie/:movie_id">
-							<MoviePage />
-						</Route>
-					</Switch>
-				</BrowserRouter>
-			</MovieTrailerProvider>
-		</div>
-	);
+  return (
+    <div className="App">
+      <MovieTrailerProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/movie/:movie_id">
+              <MoviePage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </MovieTrailerProvider>
+    </div>
+  );
 }
 
 export default App;
